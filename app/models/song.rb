a25ce71de6a689c0c21f4835d85ea96178d62e25
Jane Venter <janeanneventer@gmail.com>
2019-02-15 19:38:32 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -7,6 +7,7 @@ class Song < ActiveRecord::Base
   end
 
   def drake_made_this
-    # when this method is called it should assign the song's artist to Drake
+    # when this method is called it should assi
+    sel
   end
 end
